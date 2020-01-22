c0da7173ba54f74bbb266bee95c0cab872e1400b
pambrose <pambrose@mac.com>
2020-01-22 14:27:14 -0800

    Rename var2

--- src/main/java/org/athenian/SuperWithVariables.java
+++ src/main/java/org/athenian/SuperWithVariables.java
@@ -12,10 +12,10 @@ class Vehicle {
 }
 
 class Bike extends Vehicle {
-  int wheelCount1 = 2;
+  int wheelCount2 = 2;
 
   public void printWheels() {
-    System.out.println("Number of wheels in Bike : " + wheelCount1);
+    System.out.println("Number of wheels in Bike : " + wheelCount2);
     System.out.println("Number of wheels in Vehicle : " + super.wheelCount);
   }
 }
